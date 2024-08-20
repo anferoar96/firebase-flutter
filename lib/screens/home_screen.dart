5259d3c6c47a4810cbab4811842d782935afa7ae
aromanf96 <aromanf96@gmail.com>
2024-08-20 00:17:44 -0500

    Posts done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../providers/post_provider.dart';
+
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final postsAsyncValue = ref.watch(postsProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Posts'),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.logout))],
+      ),
+      body: postsAsyncValue.when(
+          data: (posts) {
+            return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                final post = posts[index];
+                return ListTile(
+                  title: Text(post.title),
+                  subtitle: Text(post.body),
+                );
+              },
+            );
+          },
+          loading: () => const Center(
+                child: CircularProgressIndicator(),
+              ),
+          error: (error, stackTrace) => Center(
+                child: Text('Error: $error'),
+              )),
+    );
   }
 }
